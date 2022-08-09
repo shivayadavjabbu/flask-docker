c834a14b71cdbec80e88634bbc5ea598bbede3ad
shivayadavjabbu <92553345+shivayadavjabbu@users.noreply.github.com>
2022-08-09 22:38:18 +0200

    Add files via upload

---
+++ sdn/backendserver/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, send_file, redirect, url_for, session
+import psutil
+import requests
+import json
+import time
+import subprocess
+import sys
+import os,socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def func():
+    html = "<h3>Hello {name}!</h3> <b>Hostname:</b> {hostname}<br/>"
+    return html.format(name=os.getenv("NAME", ", Kurt and Roman from VM14"), hostname=socket.gethostname())
+
+@app.route("/load")
+def load():
+    load = str(psutil.cpu_percent())
+    return load
+if __name__ == "__main__":
+        app.run(host='0.0.0.0', port=4000)
