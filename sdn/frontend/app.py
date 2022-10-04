aad57c70aee2b339ebb5f42324d82daa4ebc0600
shivayadavjabbu <92553345+shivayadavjabbu@users.noreply.github.com>
2022-10-04 21:34:03 +0200

    Update app.py

--- sdn/frontend/app.py
+++ sdn/frontend/app.py
@@ -1,22 +1,58 @@
 from flask import Flask, render_template, request, send_file, redirect, url_for, session
-import psutil
 import requests
 import json
 import time
 import subprocess
 import sys
-import os,socket
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def func():
-    html = "<h3>Hello {name}!</h3> <b>Hostname:</b> {hostname}<br/>"
-    return html.format(name=os.getenv("NAME", ", Hi Kurt and Roman from VM13"), hostname=socket.gethostname())
+ipAddressWebServer1 = "10.0.1.19"
+ipAddressWebServer2 = "10.0.1.20"
 
-@app.route("/load")
-def load():
-    load = str(psutil.cpu_percent())
-    return load
-if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=4000)
+# 0 - is RoundRobin, 1 - Weighted Round Robin
+strategy = 1
+
+# 0 - ipAddressWebServer1, 1 - ipAddressWebServer2
+roundRobin = 0
+
+loadWebServer1 = 0
+loadWebServer2 = 0
+
+
+@app.route('/', method=["GET"])
+def func():
+    response = loadBanacer(request)
+    return response
+
+
+@app.route("/changeStrategy", method = ["GET"])
+def func():
+    if strategy == 0:
+        strategy = 1
+    else:
+        strategy = 0
+    return 0
+
+def loadBanacer():
+    # Route for RoundRobin
+    if strategy == 0:
+        ipAddressWebServer = ""
+        if roundRobin == 0:
+            ipAddressWebServer = ipAddressWebServer1
+            roundRobin = 1
+        else:
+            ipAddressWebServer = ipAddressWebServer2
+            roundRobin = 0
+        response = requests.get("http://" + ipAddressWebServer + "/")
+        return response
+    if strategy == 1:
+        loadWebServer1 = requests.get("http://" + ipAddressWebServer1 + "/load")
+        loadWebServer2 = requests.get("http://" + ipAddressWebServer2 + "/load")
+        if loadWebServer1 > loadWebServer2:
+            ipAddressWebServer = loadWebServer2
+        else:
+            ipAddressWebServer = loadWebServer1
+        response = requests.get("http://" + ipAddressWebServer + "/")
+        return response
